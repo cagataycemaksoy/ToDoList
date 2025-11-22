c15e9912a3b9a8bab9372a2b0c558ae4e364420e
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-22 23:41:13 +0100

    refactored code

--- ToDoList/Task.swift
+++ ToDoList/Task.swift
@@ -17,19 +17,22 @@ class Task {
   var remindTask: Bool
   var remindingDate: Date
   
-  static var preview: ModelContainer {
-    let container = try! ModelContainer(for: Task.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
-    container.mainContext.insert(Task(todoTitle: "Learn SwiftUI", taskDescription: "Exercise SwfitUI by building projects.", taskCompleted: true, remindTask: true, remindingDate: Date.now ))
-    container.mainContext.insert(Task(todoTitle: "Cook Dinner", taskDescription: "Invite friends and cook dinner.", taskCompleted: true, remindTask: true, remindingDate: Date.now + 24*60*60))
-    return container
-  }
-  
-  init(todoTitle: String = "", taskDescription: String = "", taskCompleted: Bool = false, remindTask: Bool = false, remindingDate: Date = Calendar.current.date(byAdding: .day, value: 1, to: .now) ?? .now) {
+  init(todoTitle: String = "", taskDescription: String = "", taskCompleted: Bool = false, remindTask: Bool = false, remindingDate: Date =
+       Calendar.current.date(byAdding: .day, value: 1, to: .now) ?? .now) {
     
     self.title = todoTitle
     self.taskDescription = taskDescription
     self.taskCompleted = taskCompleted
     self.remindTask = remindTask
     self.remindingDate = remindingDate
+  }
+}
+
+extension Task {
+  static var preview: ModelContainer {
+    let container = try! ModelContainer(for: Task.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    container.mainContext.insert(Task(todoTitle: "Learn SwiftUI", taskDescription: "Exercise SwfitUI by building projects.", taskCompleted: true, remindTask: true, remindingDate: Date.now ))
+    container.mainContext.insert(Task(todoTitle: "Cook Dinner", taskDescription: "Invite friends and cook dinner.", taskCompleted: true, remindTask: true, remindingDate: Date.now + 24*60*60))
+    return container
   }
 }
